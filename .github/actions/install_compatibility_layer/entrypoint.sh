6fd947bca0f715ebe206d1a98f2f639f44755171
Bob <b.e.droge@rug.nl>
2020-08-25 20:46:16 +0200

    set prefix location, fix path to playbook

--- .github/actions/install_compatibility_layer/entrypoint.sh
+++ .github/actions/install_compatibility_layer/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
-ansible-playbook --connection=local --inventory=127.0.0.1, compatibility-layer/playbooks/install.yml
-
-ls ~/compatibility-layer
+ansible-playbook --connection=local --inventory=127.0.0.1, -e prefix_location=/tmp/gentoo ${GITHUB_WORKSPACE}/playbooks/install.yml
 
 echo "Hello $1"
 time=$(date)
