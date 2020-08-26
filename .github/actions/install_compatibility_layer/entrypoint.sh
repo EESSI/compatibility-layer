50a897b236fff36f499d9f482e3feb71356a43ad
Bob <b.e.droge@rug.nl>
2020-08-26 09:56:53 +0200

    use newer ansible

--- .github/actions/install_compatibility_layer/entrypoint.sh
+++ .github/actions/install_compatibility_layer/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
-ansible-playbook --connection=local --inventory=127.0.0.1, -e prefix_location=/tmp/gentoo ${GITHUB_WORKSPACE}/playbooks/install.yml
+echo "[cvmfsstratum0servers]\n127.0.0.1" > hosts
+ansible-playbook --connection=local --inventory=hosts -e prefix_location=/tmp/gentoo ${GITHUB_WORKSPACE}/playbooks/install.yml
 
 echo "Hello $1"
 time=$(date)
