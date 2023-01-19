312417b9aec4075ead3ded9768155448cf5e1aeb
Bob Dröge <b.e.droge@rug.nl>
2023-01-19 16:19:13 +0100

    compatibility layer build script that can be run by the EESSI bot

---
+++ bot/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+eessi_arch=x86_64 # will/should be set by bot
+eessi_version=2023.02
+eessi_repo=pilot.eessi-hpc.org
+tar_topdir=/cvmfs/${eessi_repo}/versions
+
+eessi_tmp=$(mktemp -d)
+./install_compatibility_layer.sh -a ${eessi_arch} -v ${eessi_version} -r ${eessi_repo} -c ../compat.sif
+
+# create tarball
+target_tgz=eessi-${eessi_version}-compat-linux-${eessi_arch}-$(date +%s).tar.gz
+if [ -d ${eessi_tmp}/${tar_topdir}/${eessi_version} ]; then
+  echo ">> Creating tarball ${target_tgz} from ${eessi_tmp}/${tar_topdir}..."
+  tar cfvz ${target_tgz} -C ${eessi_tmp}/${tar_topdir} ${eessi_version}
+  echo ${target_tgz} created!
+else
+  echo "Directory ${tar_topdir}/${eessi_version} was not created, not creating tarball."
+  exit 1
+fi
