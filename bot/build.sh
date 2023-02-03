1f6a01f3ae23a451bddfadb7b5c0a0c0402f247d
Bob Dröge <b.e.droge@rug.nl>
2023-02-03 13:46:11 +0100

    pick up arch requested by bot, add some checks and defaults

--- bot/build.sh
+++ bot/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-eessi_arch=x86_64 # will/should be set by bot
+cpu_target_arch=$(echo ${CPU_TARGET} | cut -d/ -f2)
+host_arch=$(uname -m)
+eessi_arch=${cpu_target_arch:-${host_arch}}
 eessi_os=linux
 eessi_version=2023.02
 eessi_repo=pilot.eessi-hpc.org
 tar_topdir=/cvmfs/${eessi_repo}/versions
+
+if [ "${eessi_arch}" != "${host_arch}" ]; then
+  echo "Requested architecture (${eessi_arch}) is different from this machine's architecture ($(uname -m))!"
+  exit 1
+fi
 
 ./install_compatibility_layer.sh -a ${eessi_arch} -v ${eessi_version} -r ${eessi_repo}
 
