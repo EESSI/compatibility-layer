25c05652a813f8a20d12c2efb828dd926a6dc6a8
Bob Dröge <b.e.droge@rug.nl>
2023-02-03 13:22:26 +0100

    include the right (sub)dir for the tarball

--- bot/build.sh
+++ bot/build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 eessi_arch=x86_64 # will/should be set by bot
+eessi_os=linux
 eessi_version=2023.02
 eessi_repo=pilot.eessi-hpc.org
 tar_topdir=/cvmfs/${eessi_repo}/versions
 
 ./install_compatibility_layer.sh -a ${eessi_arch} -v ${eessi_version} -r ${eessi_repo}
 
-# create tarball
+# create tarball -> should go into a separate script when this is supported by the bot
 target_tgz=eessi-${eessi_version}-compat-linux-${eessi_arch}-$(date +%s).tar.gz
 if [ -d ${eessi_tmp}/${tar_topdir}/${eessi_version} ]; then
   echo ">> Creating tarball ${target_tgz} from ${eessi_tmp}/${tar_topdir}..."
-  tar cfvz ${target_tgz} -C ${eessi_tmp}/${tar_topdir} ${eessi_version}
+  tar cfvz ${target_tgz} -C ${eessi_tmp}/${tar_topdir} ${eessi_version}/compat/${eessi_os}/${eessi_arch}
   echo ${target_tgz} created!
 else
   echo "Directory ${tar_topdir}/${eessi_version} was not created, not creating tarball."
